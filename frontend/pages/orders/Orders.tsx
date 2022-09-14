4fee3b8e8583ee7d6e18755060fa94f3da10323d
Vlad Rindevich <vladrin@vaadin.com>
2022-09-14 14:25:23 +0300

    refactor: align avatar with name

--- frontend/pages/orders/Orders.tsx
+++ frontend/pages/orders/Orders.tsx
@@ -1,40 +1,55 @@
 import * as React from 'react';
-import {useEffect, useState} from 'react';
-import {Avatar, Grid, GridColumn} from "react-vaadin-components";
-import {OrdersEndpoint} from "Frontend/generated/endpoints";
-import Order from "Frontend/generated/com/example/application/data/entity/Order";
+import { useEffect, useState } from 'react';
+import { Avatar, Grid, GridColumn } from 'react-vaadin-components';
+import { OrdersEndpoint } from 'Frontend/generated/endpoints';
+import Order from 'Frontend/generated/com/example/application/data/entity/Order';
+import "./Order.css";
 
 export function Orders(): React.ReactElement {
-    const [orders, setOrders] = useState(Array<Order>());
+  const [orders, setOrders] = useState(Array<Order>());
 
-    useEffect(() => {
-        (async () => {
-            const fetchedOrders = await OrdersEndpoint.getOrders();
-            setOrders(fetchedOrders);
-        })();
+  useEffect(() => {
+    (async () => {
+      const fetchedOrders = await OrdersEndpoint.getOrders();
+      setOrders(fetchedOrders);
+    })();
 
-        return () => {
-        };
-    }, []);
+    return () => {};
+  }, []);
 
-    return (
-        <div className="box-border flex flex-col p-m gap-s w-full h-full">
-            <div className="content flex gap-m h-full">
-                <Grid
-                    className="h-full"
-                    items={orders}>
-                    <GridColumn path="orderNumber" header="Order" autoWidth
-                                itemRenderer={({item}) => <span>#{item.orderNumber}</span>}/>
-                    <GridColumn path="added" header="Date added" autoWidth
-                                itemRenderer={({item}) => <span>{new Date(item.added).toDateString()}</span>}/>
-                    <GridColumn path="customer.name" header="Customer" autoWidth
-                                itemRenderer={({item}) => <span><Avatar name={item.customer.name}/> {item.customer.name}</span>}/>
-                    <GridColumn path="fulfillment" header="Fulfillment" autoWidth/>
-                    <GridColumn path="paymentStatus" header="Payment status" autoWidth/>
-                    <GridColumn path="total" header="Total" autoWidth/>
-                    <GridColumn path="currency" header="" autoWidth/>
-                </Grid>
-            </div>
-        </div>
-    )
+  return (
+    <div className="box-border flex flex-col p-m gap-s w-full h-full">
+      <div className="content flex gap-m h-full">
+        <Grid className="h-full" items={orders}>
+          <GridColumn
+            path="orderNumber"
+            header="Order"
+            autoWidth
+            itemRenderer={({ item }) => <span>#{item.orderNumber}</span>}
+          />
+          <GridColumn
+            path="added"
+            header="Date added"
+            autoWidth
+            itemRenderer={({ item }) => <span>{new Date(item.added).toDateString()}</span>}
+          />
+          <GridColumn
+            path="customer.name"
+            header="Customer"
+            autoWidth
+            itemRenderer={({ item }) => (
+              <div className="customer-name">
+                <Avatar name={item.customer.name} />
+                <span>{item.customer.name}</span>
+              </div>
+            )}
+          />
+          <GridColumn path="fulfillment" header="Fulfillment" autoWidth />
+          <GridColumn path="paymentStatus" header="Payment status" autoWidth />
+          <GridColumn path="total" header="Total" autoWidth />
+          <GridColumn path="currency" header="" autoWidth />
+        </Grid>
+      </div>
+    </div>
+  );
 }
