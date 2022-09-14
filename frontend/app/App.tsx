33f1859cd759dc2da17ebc9f89be9630db249239
Marcus Hellberg <marcus@vaadin.com>
2022-09-14 09:50:57 +0300

    remove CRM stuff

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import {AppLayout, DrawerToggle} from 'react-vaadin-components';
-import {NavLink, Outlet} from "react-router-dom";
+import { AppLayout, DrawerToggle } from 'react-vaadin-components';
+import { NavLink, Outlet } from 'react-router-dom';
 
 function App() {
-
   return (
     <AppLayout>
-
       <header slot="navbar" className="flex gap-m items-center">
         <DrawerToggle />
-        <h1 className="m-0 text-l">Hilla + React CRM</h1>
+        <h1 className="m-0 text-l">Hilla Cool Webshop</h1>
       </header>
 
       <nav slot="drawer">
-        <ul className="list-none">
-          <li><NavLink to="/">Contacts</NavLink></li>
-          <li><NavLink to="/dashboard">Dashboard</NavLink></li>
-        </ul>
+        <ul className="list-none"></ul>
       </nav>
 
-      <Outlet/>
-      
+      <Outlet />
     </AppLayout>
   );
 }
--- frontend/main.tsx
+++ frontend/main.tsx
@@ -1,31 +1,19 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import './index.css';
 import 'react-vaadin-components/dist/css/core.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Contacts from "./features/contacts/Contacts";
-import Dashboard from "./features/dashboard/Dashboard";
-import {store} from "Frontend/app/store";
-import {Provider} from "react-redux";
-import {initFromServer} from "Frontend/features/contacts/contactsSlice";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
-store.dispatch(initFromServer());
-
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App/>}>
-            <Route path="" element={<Contacts/>}/>
-            <Route path="dashboard" element={<Dashboard/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
