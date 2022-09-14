bf490333d96c49a7dbd1a9a4e6f8cf7f03d86c37
Anton Platonov <platosha@gmail.com>
2022-09-14 15:16:08 +0300

    feat: new product name and server-side validation

--- frontend/pages/products/NewProduct.tsx
+++ frontend/pages/products/NewProduct.tsx
@@ -1,28 +1,63 @@
 import * as React from 'react';
-import { useFormik } from 'formik';
+import { FormikErrors, useFormik } from 'formik';
 import { Button, FormLayout, TextArea, TextField } from 'react-vaadin-components';
 import Product from 'Frontend/generated/com/example/application/data/entity/Product';
 import { ProductEndpoint } from 'Frontend/generated/endpoints';
+import { EndpointValidationError } from '@hilla/frontend';
 
 const initialProduct: Product = {
   name: '',
   description: ''
 };
 
+type FormField<T> = {
+  name: string,
+  label: string,
+  value: T,
+  required: boolean,
+  errorMessage?: string,
+  onChange: (e: Event) => void,
+  onBlur: (e: Event) => void,
+};
+
 export function NewProduct(): React.ReactElement {
   const formik = useFormik({
     initialValues: initialProduct,
-    onSubmit: async (values, {setSubmitting}) => {
-      await ProductEndpoint.saveProduct(values);
-      setSubmitting(false);
+    onSubmit: async (values, {setSubmitting, setErrors}) => {
+      try {
+        await ProductEndpoint.saveProduct(values);
+      } catch (e: unknown) {
+        if (e instanceof EndpointValidationError) {
+          const errors: FormikErrors<Product> = {}
+          for (const error of e.validationErrorData) {
+            if (typeof error.parameterName === 'string' && error.parameterName in initialProduct) {
+              const key = error.parameterName as (string & keyof Product);
+              errors[key] = error.message;
+            }
+          }
+          setErrors(errors);
+        }
+      } finally {
+        setSubmitting(false);
+      }
     },
   });
   return <>
     <h2>New Product</h2>
     <FormLayout responsiveSteps={ [{columns: 1}] }>
+      <TextField
+        name='name'
+        label='Name'
+        required={true}
+        value={formik.values.name}
+        errorMessage={formik.errors.name}
+        onChange={formik.handleChange}
+        onBlur={formik.handleChange}
+      ></TextField>
       <TextArea
         name='description'
         label="Description"
+        value={formik.values.description}
         errorMessage={formik.errors.description}
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
