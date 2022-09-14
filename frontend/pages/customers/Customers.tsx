a3fcec8c05efed7838c3bf3f019f1ffaab20012a
Marcus Hellberg <marcus@vaadin.com>
2022-09-14 13:02:30 +0300

    WIP on customers view

--- frontend/pages/customers/Customers.tsx
+++ frontend/pages/customers/Customers.tsx
@@ -1,5 +1,36 @@
 import * as React from 'react';
+import {Avatar, Grid, GridColumn} from "react-vaadin-components";
+import {useEffect, useState} from "react";
+import {CustomersEndpoint} from "Frontend/generated/endpoints";
+import CustomerDTO from "Frontend/generated/com/example/application/data/endpoint/CustomersEndpoint/CustomerDTO";
 
 export function Customers(): React.ReactElement {
-  return <></>;
+
+  const [customers, setCustomers] = useState<CustomerDTO[]>([]);
+
+  async function fetchCustomers() {
+    setCustomers(await CustomersEndpoint.getCustomers());
+  }
+
+  useEffect(() => {
+    fetchCustomers();
+  }, []);
+
+
+  return (
+    <div className="m-xl">
+      <h2 className="text-xl my-xl">Customers</h2>
+      <Grid items={customers}>
+        <GridColumn path="added"
+                    header="Date added"
+                    autoWidth
+                    itemRenderer={({item}) => <span>{new Date(item.added).toDateString()}</span>}/>
+        <GridColumn path="name"
+                    autoWidth
+                    itemRenderer={({item}) => <Avatar name={item.name} />}/>
+        <GridColumn path="email" autoWidth/>
+        <GridColumn path="orders" autoWidth/>
+      </Grid>
+    </div>
+  );
 }
--- src/main/java/com/example/application/data/endpoint/CustomersEndpoint.java
+++ src/main/java/com/example/application/data/endpoint/CustomersEndpoint.java
@@ -1,20 +1,90 @@
 package com.example.application.data.endpoint;
 
+import com.example.application.data.entity.Customer;
 import com.example.application.data.entity.Order;
 import com.example.application.repository.CustomerRepository;
 import com.example.application.repository.DiscountRepository;
 import com.example.application.repository.GiftCardRepository;
 import com.example.application.repository.OrderRepository;
 import com.example.application.repository.PricingRepository;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.Endpoint;
 
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Endpoint
+@AnonymousAllowed
 public class CustomersEndpoint {
     private final CustomerRepository customerRepository;
 
+    public static class CustomerDTO {
+        @NotNull
+        private LocalDateTime added;
+
+        @NotBlank
+        private String name;
+
+        @NotBlank
+        private String email;
+
+        private int orders;
+        public CustomerDTO(Customer customer) {
+            this.added = customer.getAdded();
+            this.name = customer.getName();
+            this.email = customer.getEmail();
+            this.orders = customer.getOrders().size();
+        }
+
+        public CustomerDTO() {
+        }
+
+        public LocalDateTime getAdded() {
+            return added;
+        }
+
+        public void setAdded(LocalDateTime added) {
+            this.added = added;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public int getOrders() {
+            return orders;
+        }
+
+        public void setOrders(int orders) {
+            this.orders = orders;
+        }
+    }
+
     public CustomersEndpoint(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+    }
+
+    public List<CustomerDTO> getCustomers() {
+        return customerRepository
+                .findAll()
+                .stream()
+                .map(CustomerDTO::new)
+                .collect(Collectors.toList());
     }
 }
