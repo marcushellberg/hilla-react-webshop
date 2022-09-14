89f6a9dfe20cc920fbff1e5823dd5864b254f574
Luciano Vernaschi <luciano@vaadin.com>
2022-09-14 11:16:17 +0200

    Temporarily skip authentication

--- src/main/java/com/example/application/data/endpoint/OrdersEndpoint.java
+++ src/main/java/com/example/application/data/endpoint/OrdersEndpoint.java
@@ -6,6 +6,7 @@ import com.example.application.repository.DiscountRepository;
 import com.example.application.repository.GiftCardRepository;
 import com.example.application.repository.OrderRepository;
 import com.example.application.repository.PricingRepository;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.Endpoint;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class OrdersEndpoint {
         this.orderRepository = orderRepository;
     }
 
+    @AnonymousAllowed
     public List<Order> getOrders() {
         return orderRepository.findAll();
     }
